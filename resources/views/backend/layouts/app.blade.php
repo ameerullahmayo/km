667feb3206994953326a11eb421ea019a843d396
Saleem Mayo <saleemmayo371@gmail.com>
2024-06-29 21:29:48 +0500

    pull req

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Seiko Vendor App</title>
+    <title>Kitchen's Masala</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
